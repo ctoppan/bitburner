15f6f32aa4a759fa93f293db8211df317b572a26
Chris Toppan <chris@toppan.org>
2026-04-03 05:39:57 -0500

    Update runHacking.js

--- src/hacking/main/runHacking.js
+++ src/hacking/main/runHacking.js
@@ -1,30 +1,4 @@
-function localeHHMMSS(ms = 0) {
-  if (!ms) {
-    ms = new Date().getTime()
-  }
-
-  return new Date(ms).toLocaleTimeString()
-}
-
+/** @param {NS} ns **/
 export async function main(ns) {
-  ns.tprint(`[${localeHHMMSS()}] Starting runHacking.js`)
-
-  let hostname = ns.getHostname()
-
-  if (hostname !== 'home') {
-    throw new Exception('Run the script from home')
-  }
-
-  const homeRam = ns.getServerMaxRam('home')
-
-  if (homeRam >= 32) {
-    ns.tprint(`[${localeHHMMSS()}] Spawning spider.js`)
-    await ns.run('/hacking/main/spider.js', 1, '/hacking/main/mainHack.js')
-    await ns.sleep(3000)
-    ns.tprint(`[${localeHHMMSS()}] Spawning playerServers.js`)
-    ns.spawn('/hacking/playerServers.js', 1)
-  } else {
-    ns.tprint(`[${localeHHMMSS()}] Spawning spider.js`)
-    ns.spawn('/hacking/main/spider.js', 1, '/hacking/main/mainHack.js')
-  }
+    ns.tprint("runHacking.js disabled (orchestrator in control)");
 }
